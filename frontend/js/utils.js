2dfedce405bd145a0368b4ac9038c4b0df467435
Faithyla <faithejukwa@gmail.com>
2024-11-27 16:22:51 -0800

    second commit

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://taskmanager-backend-zsph.onrender.com'; // Base URL for the API
+const apiUrl = 'https://task-manager-c83b.onrender.com'; // Base URL for the API
 
 // Generic GET request
 async function getData(endpoint, token = '') {
